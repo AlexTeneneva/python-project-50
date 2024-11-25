66fccd83129fd0e9c7de804e3c2bd6c49edb3a23
Alexandra <a.teneneva@gmail.com>
2024-11-25 17:00:27 +0300

    step 5, ver 6

--- gendiff/data.py
+++ gendiff/data.py
@@ -3,10 +3,10 @@ import yaml
 
 
 def get_data(path):
-	with open(path, encoding='utf8'):
-		if path.endswith('.json'):
-			return json.load(open(path))
-		elif path.endswith('.yml') or path.endswith('.yaml'):
-			return yaml.safe_load(path)
-		else:
-			return "Not found such extension"
+    with open(path, encoding='utf8'):
+        if path.endswith('.json'):
+            return json.load(open(path))
+        elif path.endswith('.yml') or path.endswith('.yaml'):
+            return yaml.safe_load(path)
+        else:
+            return "Not found such extension"
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,7 @@
 from gendiff.gendiff import generate_diff as gendiff
 from gendiff.cli import invitation
 
+
 def main():
     first_file, second_file, format = invitation()
     print(gendiff(first_file, second_file, format))
