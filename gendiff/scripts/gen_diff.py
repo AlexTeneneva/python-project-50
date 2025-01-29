5bc6d89854ee4e82305185aac81fe845cd737ca7
AlexTeneneva <a.teneneva@gmail.com>
2025-01-29 20:12:31 +0300

    uv

---
+++ gendiff/scripts/gen_diff.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests",
+# ]
+# ///
+from gendiff.gendiff import generate_diff as gendiff
+from gendiff.cli import invitation
+
+
+def main():
+    first_file, second_file, format = invitation()
+    print(gendiff(first_file, second_file, format))
+
+
+if __name__ == '__main__':
+    main()
